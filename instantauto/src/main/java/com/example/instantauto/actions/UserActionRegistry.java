401009f3272fc19c841fffd4f0d4b75a3e7bf793
BoscoTU <chungchaktu@gmail.com>
2026-08-14 13:15:09 -0700

    debug: add config in strafe to

--- instantauto/src/main/java/com/example/instantauto/actions/UserActionRegistry.java
+++ instantauto/src/main/java/com/example/instantauto/actions/UserActionRegistry.java
@@ -163,14 +163,6 @@ public class UserActionRegistry {
             MetaAction meta = registry.get(name.toUpperCase());
 
             if (meta == null) return null;
-
-//            // Try to resolve as a variable first
-//            MetaFieldRegistry.ConfigEntry<?> variableEntry = MetaFieldRegistry.getEntry(paramsLine);
-//            if (variableEntry != null && variableEntry.getValue() != null) {
-//                paramObject = variableEntry.getValue();
-//                System.out.println("Variable Entry: " + paramObject + " " + name);
-//
-//            }
             return meta.create(paramsLine);
         } else {
             // Action without parameters
